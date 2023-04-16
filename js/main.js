0d866c6eedb0339f90a45020eb6c93d507985d49
ahmedsalah2002 <ahmed147salah@gmail.com>
2023-04-16 18:07:14 +0200

    first publish for time app

---
+++ js/main.js
@@ -0,0 +1,44 @@
+    
+let api="https://worldtimeapi.org/api/timezone/";
+async function get(){
+    let repo= await fetch (api);
+    let data=await repo.json();
+    console.log(data);
+    let time=document.getElementById("time");
+for(let i=0;i<data.length;i++){
+    let opt=document.createElement("option");
+    opt.value=data[i]
+    
+    opt.innerHTML=data[i]
+    time.appendChild(opt)
+}
+
+
+    let tz;
+   
+    
+    time.onchange=function(){
+        tz=time.value
+        async function show(){
+        let repo=await fetch(api+tz)
+        let data=await repo.json();
+        function dspTz(){
+            let clock= document.querySelector(".clock");
+            clock.innerHTML=data.datetime.slice((data.datetime.indexOf("T")+1),data.datetime.indexOf("."))
+         }
+         dspTz()
+        }
+        
+          
+            setInterval(() => {
+                show()
+            }, 1000);
+    }
+
+}
+
+get()
+
+
+
+
